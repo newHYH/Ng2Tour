2f39bf4285e55d97742848fc1d14efdd3b420e69
newHYH <houge1422841169@qq.com>
2017-02-04 14:18:52 +0800

    创建httpp请求数据使用inMemoryDataService等

---
+++ angular-tour-of-heros/app/in-memory-data.service.ts
@@ -0,0 +1,20 @@
+import { InMemoryDataService } from 'angular-in-memory-web-api';
+
+export class InMemoryDataService implements InMemoryDataService{
+    createDb(){
+        let heroes = [
+            { id: 11, name: "Mr.one" },
+            { id: 12, name: "Mr.two" },
+            { id: 13, name: "Mr.three" },
+            { id: 14, name: "Mr.four" },
+            { id: 15, name: "Mr.five" },
+            { id: 16, name: "Mr.six" },
+            { id: 17, name: "Mr.seven" },
+            { id: 18, name: "Mr.eight" },
+            { id: 19, name: "Mr.nine" },
+
+        ];
+        return {heroes};
+    }
+
+}
